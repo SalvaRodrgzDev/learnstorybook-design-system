01f5d38ddb42e7b144b33c3514bd1803266e2856
Salva Rodriguez <tcolsrr@gmail.com>
2021-05-05 15:17:57 +0200

    controls added

--- src/shared/global.js
+++ src/shared/global.js
@@ -1,7 +1,15 @@
-import { createGlobalStyle, css } from 'styled-components';
-import { color, typography } from './styles';
+import {
+	createGlobalStyle,
+	css
+} from 'styled-components';
+import {
+	color,
+	typography
+} from './styles';
 
-export const bodyStyles = css`
+export const fontUrl = 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900';
+
+export const bodyStyles = css `
   font-family: ${typography.type.primary};
   font-size: ${typography.size.s3}px;
   color: ${color.darkest};
@@ -111,7 +119,7 @@ export const bodyStyles = css`
   }
 `;
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle `
  body {
    ${bodyStyles}
  }
